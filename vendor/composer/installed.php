55c6319e2e0109773f0698693ecf663a3560dbb2
goreliu <goreliu@tencent.com>
2021-09-04 00:16:06 +0800

    add

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bdae547b7c9fbbccacda1a5662f661550e8e2d84',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bdae547b7c9fbbccacda1a5662f661550e8e2d84',
+            'dev_requirement' => false,
+        ),
+    ),
+);
